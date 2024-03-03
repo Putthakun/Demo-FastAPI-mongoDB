7dab91688ccd9993f4fd05e057fb9652b68a3dfa
s6404062610171 <139251998+s6404062610171@users.noreply.github.com>
2024-03-04 06:02:16 +0700

    update main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel
@@ -43,5 +44,6 @@ async def create_Patient_information(patient_information:Patient_information):
         "congenitaldisease" : patient_information.congenitaldisease,
         
     }
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
